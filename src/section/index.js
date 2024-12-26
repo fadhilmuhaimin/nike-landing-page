dd7bbca774d9fb39cdadfa808d7c3548384b59e2
fadhilmuhaimin <fadilmuhaimin88@gmail.com>
2024-12-26 15:25:39 +0800

    first commit

---
+++ src/section/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffer";
+import CustomerReview from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReview,
+    Footer,
+    Subscribe
+}
